c24e225ad9658affa927018deecae67ae2f8bc08
Jared Eiseman and Kristen McKee <kristen@>
2017-04-24 09:58:51 -0700

    add functions and reverse for gallons to liters, tablespoon to teaspoon, cups to ounces

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+var gallonsToLiters = function (num) {
+  return num * 3.79;
+};
+
+var litersToGallons = function (num) {
+  return num / 3.79;
+};
+
+var tablespoonToTeaspoon = function (num) {
+  return num * 1/3;
+};
+
+var teaspoonToTablespoon = function (num) {
+  return num * 3;
+};
+
+var cupsToOunces = function (num) {
+  return num * 8;
+};
+
+var ouncesToCups = function (num) {
+  return num / 8;
+};
